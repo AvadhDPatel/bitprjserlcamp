2832330541f2947dd2b2299893792f88530ccb36
Avadh Patel <avadhpatel@Avadhs-MacBook-Air-2.local>
2022-04-25 20:50:41 -0500

    added running late function that return statement based on time

--- late.js
+++ late.js
@@ -1,1 +1,14 @@
 // start coding your function here!
+date = new Date()
+
+function running_late(date) {
+    if (date >= "December 17, 1995 03:24:00"){
+        return "It is late!"
+    }
+    else{
+        return "It is still early!"
+    }
+    
+} 
+
+exports.running_late = running_late;
