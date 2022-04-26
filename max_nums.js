613d4ee9e5928648218d7ce1cb35a4e15cfbe21f
Avadh Patel <avadhpatel@Avadhs-MacBook-Air-2.local>
2022-04-26 12:08:00 -0500

    added function find_max with array as argument to return maximum number in an array

--- max_nums.js
+++ max_nums.js
@@ -1,7 +1,9 @@
 // start coding your function here!
 function find_max(arr){
 
-    return Math.max(arr)        //returns the maximum number in an array.
+    maxn = Math.max(arr) //maximum number
+
+    return maxn        //returns the maximum number in an array.
 } 
 
 exports.find_max = find_max;
