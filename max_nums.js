ed7b8a36fdd4aedae27233ff7eb925e206c89c64
Avadh Patel <93529099+AvadhDPatel@users.noreply.github.com>
2022-04-26 20:21:46 -0500

    Update max_nums.js

--- max_nums.js
+++ max_nums.js
@@ -1,16 +1,8 @@
 // start coding your function here!
 function find_max(arr)
 {
-
-    var maxn = 0;        //maximum number
-  
-    for(var i=0;i<arr.length;i++)
-    {
-        if(arr[i].a>maxn)
-        {
-        maxn = arr[i].a;
-       }
-    }
+    maxn = arr.max()
+    
     return maxn; //returns the maximum number in an array.
 }
 
